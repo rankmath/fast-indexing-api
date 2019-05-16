85be3ecfc44b2cbb50347a04a5d33de5c7cd3c3b
balazsp <balazs.p@mythemeshop.com>
2019-05-16 06:59:33 +0200

    Rebranded to RM

--- mts-google-indexing-api.php
+++ mts-google-indexing-api.php
@@ -1,26 +1,26 @@
 <?php
 /*
- * Plugin Name: MTS Google Indexing API
- * Plugin URI: https://mythemeshop.com
+ * Plugin Name: Rank Math Google Indexing API
+ * Plugin URI: https://rankmath.com
  * Description: Crawl pages instantly with the indexing API.
  * Version: 1.1
- * Author: MyThemeShop
- * Author URI: https://mythemeshop.com
+ * Author: Rank Math
+ * Author URI: https://rankmath.com
  * License: GPLv2
  */
 
 defined('ABSPATH') or die;
 
-class MTS_GIAPI {
+class RM_GIAPI {
 
     function __construct() {
         add_action( 'admin_init', array( $this, 'admin_init' ) );
         add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
         add_action( 'admin_menu', array( $this, 'admin_menu' ) );
-        add_action( 'wp_ajax_mts_giapi',array( $this,'ajax_mts_giapi' ) );
-        add_action( 'wp_ajax_mts_giapi_deauth',array( $this,'ajax_mts_giapi_deauth' ) );
+        add_action( 'wp_ajax_rm_giapi',array( $this,'ajax_rm_giapi' ) );
+        add_action( 'wp_ajax_rm_giapi_deauth',array( $this,'ajax_rm_giapi_deauth' ) );
         
-        $post_types = apply_filters( 'mtsgiapi_post_types', array( 'post', 'page' ) );
+        $post_types = apply_filters( 'rmgiapi_post_types', array( 'post', 'page' ) );
         foreach ( $post_types as $pt ) {
             add_filter( $pt.'_row_actions', array( $this, 'send_to_api_link' ), 10, 2 );
         }
@@ -29,12 +29,12 @@ class MTS_GIAPI {
     }
 
     function send_to_api_link( $actions, $post ) {
-        if ( ! current_user_can( apply_filters( 'mtsgiapi_capability', 'manage_options' ) ) ) {
+        if ( ! current_user_can( apply_filters( 'rmgiapi_capability', 'manage_options' ) ) ) {
             return $actions;
         }
         $nonce = wp_create_nonce( 'giapi-action' );
-        $actions['mtsgiapi_update'] = '<a href="' . admin_url( 'tools.php?page=mts-giapi&apiaction=update&_wpnonce='.$nonce.'&apiurl='.rawurlencode( get_permalink( $post) ) ) . '" class="mtsgiapi-link mtsgiapi_update">' . __('Indexing API: Update', 'mts-giapi') . '</a>';
-        $actions['mtsgiapi_getstatus'] = '<a href="' . admin_url( 'tools.php?page=mts-giapi&apiaction=getstatus&_wpnonce='.$nonce.'&apiurl='.rawurlencode( get_permalink( $post) ) ) . '" class="mtsgiapi-link mtsgiapi_update">' . __('Indexing API: Get Status', 'mts-giapi') . '</a>';
+        $actions['rmgiapi_update'] = '<a href="' . admin_url( 'tools.php?page=rm-giapi&apiaction=update&_wpnonce='.$nonce.'&apiurl='.rawurlencode( get_permalink( $post) ) ) . '" class="rmgiapi-link rmgiapi_update">' . __('Indexing API: Update', 'rm-giapi') . '</a>';
+        $actions['rmgiapi_getstatus'] = '<a href="' . admin_url( 'tools.php?page=rm-giapi&apiaction=getstatus&_wpnonce='.$nonce.'&apiurl='.rawurlencode( get_permalink( $post) ) ) . '" class="rmgiapi-link rmgiapi_update">' . __('Indexing API: Get Status', 'rm-giapi') . '</a>';
         return $actions;
     }
     
@@ -42,8 +42,8 @@ class MTS_GIAPI {
         load_plugin_textdomain( 'mythemeshop-giapi', false, dirname( plugin_basename( __FILE__ ) ) . '/language/' ); 
     }
     
-    function ajax_mts_giapi() {
-        if ( ! current_user_can( apply_filters( 'mtsgiapi_capability', 'manage_options' ) ) ) {
+    function ajax_rm_giapi() {
+        if ( ! current_user_can( apply_filters( 'rmgiapi_capability', 'manage_options' ) ) ) {
             die('0');
         }
 
@@ -101,7 +101,7 @@ class MTS_GIAPI {
 
     function admin_menu() {
         // Add the new admin menu and page and save the returned hook suffix    
-        $this->menu_hook_suffix = add_management_page(__('Google Indexing API', 'mts-giapi'), __('Indexing API', 'mts-giapi'), apply_filters( 'mtsgiapi_capability', 'manage_options' ), 'mts-giapi', array( $this, 'show_ui' ) );
+        $this->menu_hook_suffix = add_management_page(__('Google Indexing API', 'rm-giapi'), __('Indexing API', 'rm-giapi'), apply_filters( 'rmgiapi_capability', 'manage_options' ), 'rm-giapi', array( $this, 'show_ui' ) );
         add_action( 'load-' . $this->menu_hook_suffix , array( $this, 'ui_onload' ) );
     
     }
@@ -120,23 +120,23 @@ class MTS_GIAPI {
         ?>
         <div class="wrap">
             <h2><?php echo get_admin_page_title(); ?></h2>
-            <form id="mts-giapi" class="wpform" method="post">
-                <label for="giapi-url"><?php _e('URLs (one per line, up to 100):', 'mts-giapi'); ?></label><br>
+            <form id="rm-giapi" class="wpform" method="post">
+                <label for="giapi-url"><?php _e('URLs (one per line, up to 100):', 'rm-giapi'); ?></label><br>
                 <textarea name="url" id="giapi-url" class="regular-text code" style="min-width: 600px;" rows="5"><?php echo esc_textarea( home_url( '/' ) ); ?></textarea>
                 <br><br>
-                <label><?php _e('Action:', 'mts-giapi'); ?></label><br>
-                <label><input type="radio" name="api_action" value="update" checked="checked" class="giapi-action"> <?php _e('Publish/update', 'mts-giapi'); ?></label><br>
-                <label><input type="radio" name="api_action" value="remove" class="giapi-action"> <?php _e('Remove', 'mts-giapi'); ?></label><br>
-                <label><input type="radio" name="api_action" value="getstatus" class="giapi-action"> <?php _e('Get status', 'mts-giapi'); ?></label><br><br>
-                <input type="submit" id="giapi-submit" class="button button-primary" value="<?php esc_attr_e('Send to API', 'mts-giapi'); ?>">
+                <label><?php _e('Action:', 'rm-giapi'); ?></label><br>
+                <label><input type="radio" name="api_action" value="update" checked="checked" class="giapi-action"> <?php _e('Publish/update', 'rm-giapi'); ?></label><br>
+                <label><input type="radio" name="api_action" value="remove" class="giapi-action"> <?php _e('Remove', 'rm-giapi'); ?></label><br>
+                <label><input type="radio" name="api_action" value="getstatus" class="giapi-action"> <?php _e('Get status', 'rm-giapi'); ?></label><br><br>
+                <input type="submit" id="giapi-submit" class="button button-primary" value="<?php esc_attr_e('Send to API', 'rm-giapi'); ?>">
             </form>
             <div style="display: none;" id="giapi-response-wrapper">
                 <br><hr><br>
-                <textarea id="giapi-response" class="large-text code" rows="10" placeholder="<?php esc_attr_e('Response...', 'mts-giapi'); ?>"></textarea>
+                <textarea id="giapi-response" class="large-text code" rows="10" placeholder="<?php esc_attr_e('Response...', 'rm-giapi'); ?>"></textarea>
             </div>
             <br>
             <br>
-            <p class="" style="line-height: 1.8"><a href="https://developers.google.com/search/apis/indexing-api/v3/quota-pricing" target="_blank"><strong><?php _e('API Limits:', 'mts-giapi'); ?></strong></a><br>
+            <p class="" style="line-height: 1.8"><a href="https://developers.google.com/search/apis/indexing-api/v3/quota-pricing" target="_blank"><strong><?php _e('API Limits:', 'rm-giapi'); ?></strong></a><br>
             <code>PublishRequestsPerDayPerProject = <strong>200</strong></code><br>
             <code>RequestsPerMinutePerProject = <strong>600</strong></code><br>
             <code>MetadataRequestsPerMinutePerProject = <strong>180</strong></code></p>
@@ -164,7 +164,7 @@ class MTS_GIAPI {
                     $responseTextarea.val(info + "\n" + current);
                 };
 
-                $('#mts-giapi').submit(function(event) {
+                $('#rm-giapi').submit(function(event) {
                     event.preventDefault();
                     $submitButton.attr('disabled', 'disabled');
                     $('#giapi-response-wrapper').show();
@@ -172,7 +172,7 @@ class MTS_GIAPI {
                         url: ajaxurl,
                         type: 'POST',
                         dataType: 'json',
-                        data: { action: 'mts_giapi', url: $urlField.val(), api_action: $actionRadio.filter(':checked').val() },
+                        data: { action: 'rm_giapi', url: $urlField.val(), api_action: $actionRadio.filter(':checked').val() },
                     })
                     .done(function(data) {
                         logResponse(JSON.stringify(data, null, 2));
@@ -187,8 +187,8 @@ class MTS_GIAPI {
 
                 <?php if ( ! empty( $_GET['apiaction'] ) && ! empty( $_GET['apiurl'] ) && wp_verify_nonce( $_GET['_wpnonce'], 'giapi-action' ) ) { ?>
                     $('#giapi-url').val('<?php echo esc_url_raw( $_GET['apiurl'] ); ?>');
-                    $('#mts-giapi').find('input.giapi-action[value="<?php echo sanitize_title( $_GET['apiaction'] ); ?>"]').prop('checked', true);
-                    $('#mts-giapi').submit();
+                    $('#rm-giapi').find('input.giapi-action[value="<?php echo sanitize_title( $_GET['apiaction'] ); ?>"]').prop('checked', true);
+                    $('#rm-giapi').submit();
                 <?php } ?>
             });        
         </script>
@@ -202,4 +202,4 @@ class MTS_GIAPI {
 
 }
 
-$mts_giapi = new MTS_GIAPI();
+$rm_giapi = new RM_GIAPI();
