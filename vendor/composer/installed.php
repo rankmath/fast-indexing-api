2c8eda78ee46e5c25bd9c11c5ca9856c6d7392f6
pratikrm <pratik.d@rankmath.com>
2023-11-15 14:38:21 +0530

    Version bump

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'dedbee6ca245e75c6eff031ca98e3f368364dff9',
+        'reference' => '61c021e7e99b55da8115fafdb4ac6bcb128b4372',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'dedbee6ca245e75c6eff031ca98e3f368364dff9',
+            'reference' => '61c021e7e99b55da8115fafdb4ac6bcb128b4372',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
